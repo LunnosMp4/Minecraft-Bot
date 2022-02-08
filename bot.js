c3378a3a1b721386b81f59c87c09fbc0f037c471
LunnosMp4 <loic.tisseyre@epitech.eu>
2022-02-08 22:47:13 +0100

    add commands : fight, guard, s[command], follow, come, help

--- bot.js
+++ bot.js
@@ -1,8 +1,16 @@
+/*//*//*//*//*//*//*//*//*//*//*//*/
+/*/ Made by Lunnos and Doud Irow /*/
+/*//*//*//*//*//*//*//*//*//*//*//*/
+
+//const comeToMe = require('./command/come.js')
 const mineflayer = require('mineflayer')
 const { pathfinder, Movements, goals } = require('mineflayer-pathfinder')
+const GoalFollow = goals.GoalFollow
 const { GoalNear } = require('mineflayer-pathfinder').goals
 const pvp = require('mineflayer-pvp').plugin
 const collectBlock = require('mineflayer-collectblock').plugin
+const toolPlugin = require('mineflayer-tool').plugin
+const armorManager = require('mineflayer-armor-manager')
 
 if (process.argv.length < 2 || process.argv.length > 6) {
     console.log('Usage : node bot.js <name> <host> <password> <port>')
@@ -13,94 +21,190 @@ const bot = mineflayer.createBot({
     username: process.argv[2] || 'Pato',
     host: process.argv[3],
     port: process.argv[4]
-    //password: process.argv[5],
 })
 
+bot.loadPlugin(toolPlugin)
 bot.loadPlugin(collectBlock)
+bot.loadPlugin(armorManager)
 bot.loadPlugin(pathfinder)
 bot.loadPlugin(pvp)
+
+
+
+//////////////////////////////// Collect Item //////////////////////////////////
+bot.on('playerCollect', (collector, itemDrop) => {
+    if (collector !== bot.entity) return
+    setTimeout(() => {
+      const sword = bot.inventory.items().find(item => item.name.includes('sword'))
+      if (sword) bot.equip(sword, 'hand')
+    }, 150)
+})
+
+bot.on('playerCollect', (collector, itemDrop) => {
+    if (collector !== bot.entity) return
+    setTimeout(() => {
+      const shield = bot.inventory.items().find(item => item.name.includes('shield'))
+      if (shield) bot.equip(shield, 'off-hand')
+    }, 250)
+})
+///////////////////////////////////////////////////////////////////////////////
+
+
+
+//////////////////////////////// Protect Function//////////////////////////////
+let guardPos = null
+
+function guardArea (pos) {
+  guardPos = pos.clone()
+
+  if (!bot.pvp.target) {
+    moveToGuardPos()
+  }
+}
+
+function stopGuarding () {
+  guardPos = null
+  bot.pvp.stop()
+  bot.pathfinder.setGoal(null)
+}
+
+function moveToGuardPos () {
+    const mcData = require('minecraft-data')(bot.version)
+    bot.pathfinder.setMovements(new Movements(bot, mcData))
+    bot.pathfinder.setGoal(new goals.GoalBlock(guardPos.x, guardPos.y, guardPos.z))
+}
+
+bot.on('stoppedAttacking', () => {
+    if (guardPos) {
+      moveToGuardPos()
+    }
+})
+///////////////////////////////////////////////////////////////////////////////
+
+
+
+//////////////////////////////// Protect Run //////////////////////////////
+bot.on('physicTick', () => {
+    if (bot.pvp.target) return
+    if (bot.pathfinder.isMoving()) return
+
+    const entity = bot.nearestEntity()
+    if (entity) bot.lookAt(entity.position.offset(0, entity.height, 0))
+})
+
+  bot.on('physicTick', () => {
+    if (!guardPos) return
+
+    const filter = e => e.type === 'mob' && e.position.distanceTo(bot.entity.position) < 16 &&
+                        e.mobType !== 'Armor Stand'
+
+    const entity = bot.nearestEntity(filter)
+    if (entity) {
+      bot.pvp.attack(entity)
+    }
+})
+///////////////////////////////////////////////////////////////////////////////
+
+
 
 bot.once('spawn', () => {
     const mcData = require('minecraft-data')(bot.version)
     const defaultMove = new Movements(bot, mcData)
-    bot.on('chat', (username, message) => {
-        if (username === bot.username) return
-            const target = bot.players[username] ? bot.players[username].entity : null
-        if (message === 'help') { //help command
-            bot.chat('Commands :')
-            bot.chat('Attack -> El pato attack the nearest entity')
-            bot.chat('Come -> El pato comes to you')
-            bot.chat('Fight me -> El pato follow you and attack you until your death')
-            bot.chat('stop-> El pato stop fight you')
-        } else if (message === 'attack')
-            attackEntity()
-        else if (message === 'come') { //come command
-            comeToMe(target, defaultMove)
-        } else if (message === 'fight me') { //fight command
-            const player = bot.players[username]
-            if (!player) {
-              bot.chat("I can\'t see you.")
-              return
-            }
-            bot.pvp.attack(player.entity)
-        } else if (message === 'stop') //stop command
-            bot.pvp.stop()
-        else { //do collector -> collect <block>
-            const args = message.split(' ')
-            if (args[0] !== 'collect') return
-
-            let count = 1
-            if (args.length === 3) count = parseInt(args[1])
-
-            let type = args[1]
-            if (args.length === 3) type = args[2]
-
-            const blockType = mcData.blocksByName[type]
-            if (!blockType) {
-                bot.chat(`"I don't know any blocks named ${type}.`)
-                return
-            }
-
-            const blocks = bot.findBlocks({
-                matching: blockType.id,
-                maxDistance: 64,
-                count: count
-            })
-
-            if (blocks.length === 0) {
-                bot.chat("I don't see that block nearby.")
-                return
-            }
-
-            const targets = []
-            for (let i = 0; i < Math.min(blocks.length, count); i++) {
-                targets.push(bot.blockAt(blocks[i]))
-            }
-
-            bot.chat(`Found ${targets.length} ${type}(s)`)
-
-            bot.collectBlock.collect(targets, err => {
-                if (err) {
-                bot.chat(err.message)
-                console.log(err)
-                } else {
-                bot.chat('Done')
-                }
-            })
-        }
-    })
 })
 
-function comeToMe(target, defaultMove) {
-    if (!target) {
-        bot.chat('I don\'t see you !')
-        return
-    }
-    const p = target.position
-    bot.pathfinder.setMovements(defaultMove)
-    bot.pathfinder.setGoal(new GoalNear(p.x, p.y, p.z, 1))
-}
+bot.on('chat', (username, message) => {
+    const mcData = require('minecraft-data')(bot.version)
+    const defaultMove = new Movements(bot, mcData)
 
+    const args = message.split(' ')
+    if (username === bot.username) return
+    const target = bot.players[username] ? bot.players[username].entity : null
+    if (args.length === 2)
+        username = args[1]
+    if (args[0] === 'help') { //help command
+        bot.chat('Commands :')
+        bot.chat('  Come -> El pato comes to you')
+        bot.chat('  Fight me -> El pato follow you and attack you until your death')
+        bot.chat('  Follow -> Follow the person')
+        bot.chat('  Guard -> El pato attack the nearest entity')
+        bot.chat('  s[command]-> El pato stop [command]')
+    } else if (args[0] === 'attack')
+        attackEntity()
+    else if (args[0] === "follow" || args[0] === "flw")
+        followPlayer(username)
+    else if (args[0] === "testtt")
+        bot.chat(target)
+    else if (args[0] === "sflw")
+        bot.pathfinder.stop()
+    else if (args[0] === 'come') //come command
+        comeToMe(username)
+    else if (args[0] === 'guard') {
+        const player = bot.players[username]
+        if (!player) {
+          bot.chat("I can't see you.")
+          return
+        }
+        bot.chat('I will guard that location.')
+        guardArea(player.entity.position)
+    } else if (args[0] === 'fight') {
+        const player = bot.players[username]
+        if (!player) {
+          bot.chat("I can't see you.")
+          return
+        }
+        bot.chat('Prepare to fight!')
+        bot.pvp.attack(player.entity)
+      }
+      if (args[0] === 'sguard') {
+        bot.chat("I'm friendly now !")
+        stopGuarding()
+      } else if (args[0] === 'sfight') //stop pvp command
+        bot.pvp.stop()
+    else { //do collector -> collect <block>
+        const args = message.split(' ')
+        if (args[0] !== 'collect') return
+        let count = 1
+
+        if (args.length === 3) count = parseInt(args[1])
+            let type = args[1]
+        if (args.length === 3) type = args[2]
+            const blockType = mcData.blocksByName[type]
+        if (!blockType) {
+            bot.chat(`"I don't know any blocks named ${type}.`)
+            return
+        }
+
+        const blocks = bot.findBlocks({
+            matching: blockType.id,
+            maxDistance: 64,
+            count: count
+        })
+
+        if (blocks.length === 0) {
+            bot.chat("I don't see that block nearby.")
+            return
+        }
+
+        const targets = []
+        for (let i = 0; i < Math.min(blocks.length, count); i++) {
+            targets.push(bot.blockAt(blocks[i]))
+        }
+
+        bot.chat(`Found ${targets.length} ${type}(s)`)
+
+        bot.collectBlock.collect(targets, err => {
+            if (err) {
+            bot.chat(err.message)
+            console.log(err)
+            } else {
+            bot.chat('Done')
+            }
+        })
+    }
+})
+
+
+/////////////////////// Attack Nearly Player////////////////////////
 function attackEntity() {
     const entity = bot.nearestEntity()
     if (!entity) {
@@ -110,6 +214,46 @@ function attackEntity() {
         bot.attack(entity)
     }
 }
+//////////////////////////////////////////////////////////////////
+
+
+
+////////////////////////// Come To Player/////////////////////////
+function comeToMe(username) {
+    const target = bot.players[username]
+    if (!target) {
+        bot.chat('I don\'t see you !')
+        return
+    }
+    const mcData = require('minecraft-data')(bot.version)
+    const defaultMove = new Movements(bot, mcData)
+    bot.pathfinder.setMovements(defaultMove)
+
+    const goal = new GoalFollow(target.entity, 1)
+    bot.pathfinder.setGoal(goal, false)
+}
+//////////////////////////////////////////////////////////////////
+
+
+
+////////////////////////// Follow Player//////////////////////////
+function followPlayer(username) {
+    const playerCI = bot.players[username]
+
+    if (!playerCI) {
+        bot.chat('I don\'t see you !')
+        return
+    }
+    const mcData = require('minecraft-data')(bot.version)
+    const defaultMove = new Movements(bot, mcData)
+    bot.pathfinder.setMovements(defaultMove)
+
+    const goal = new GoalFollow(playerCI.entity, 1)
+    bot.pathfinder.setGoal(goal, true)
+}
+//////////////////////////////////////////////////////////////////
+
+
 
 bot.on('kicked', console.log)
 bot.on('error', console.log)
